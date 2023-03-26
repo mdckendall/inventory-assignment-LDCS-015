0e811e24e581bcb488cd75c12d8d1f05857ca369
LDCS-015 <luisdavidcamacho15@gmail.com>
2023-03-26 17:55:24 -0400

    Main TD: Finish Case 4 and 5

--- Main.java
+++ Main.java
@@ -2,11 +2,86 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class Inventory {
+	String name;
+	String serialNum;
+	int val;
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setVal(int val) {
+		this.val = val;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSerialNum() {
+		return serialNum;
+	}
+
+	public int getVal() {
+		return val;
+	}
+
+	public Inventory(String name, String serialNum, int val) {
+		this.name = name;
+		this.serialNum = serialNum;
+		this.val = val;
+	}
 }
 
 class Main {
 	public static void main(String[] args) {
+		Scanner std = new Scanner(System.in);
+		ArrayList<Inventory> inv = new ArrayList<Inventory>();
+
+		int check = 0;
+
+		while (check != 5) {
+			System.out.println("Press 1 to add an item.");
+			System.out.println("Press 2 to delete an item.");
+			System.out.println("Press 3 to update an item.");
+			System.out.println("Press 4 to show all the item.");
+			System.out.println("Press 5 to quit the program.");
+
+			std.nextInt();
+			switch (check) {
+				case 1:
+					System.out.println("Enter the name:");
+					String name = std.nextLine();
+					System.out.println("Enter the serial number:");
+					String serialNum = std.nextLine();
+					System.out.println("Enter the value in dollars (whole number):");
+					int val = std.nextInt();
+					Inventory item = new Inventory(name, serialNum, val);
+					inv.add(item);
+					break;
+				case 2:
+					System.out.println("Enter the serial number of the item to delete:");
+					String temp = std.nextLine();
+					inv.remove(inv.indexOf(temp));
+					break;
+				case 3:
+					System.out.println("Enter the serial number of the item to change:");
+					String serialNum2 = std.nextLine();
+					int index = inv.indexOf(serialNum2);
+					System.out.println("Enter the new name:");
+					String name2 = std.nextLine();
+					System.out.println("Enter the new value in dollars (whole number):");
+					int val2 = std.nextInt();
+					inv.get(index).setName(name2);
+					inv.get(index).setVal(val2);
+					break;
+				case 4: 
+					for(int i = 0; i < inv.size(); i++);
+						inv.get(i);
+					
+			}
+
+		}
 
 	}
 }
